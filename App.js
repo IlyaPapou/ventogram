b9a88c049ca05249c494f48e76f1e68914749013
Ilya <lya.papou@redvike.com>
2021-01-02 18:52:20 +0300

    Auth hadnling added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, StyleSheet } from 'react-native';
 import * as firebase from 'firebase';
 import * as Sentry from 'sentry-expo';
 import LandingScreen from './components/auth/Landing';
@@ -19,16 +20,62 @@ Sentry.init({
   debug: true, // to `false` in production.
 });
 
-const Stack = createStackNavigator();
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const Stack = createStackNavigator();
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          isSignedIn: false,
+          isLoaded: true,
+        });
+      } else {
+        this.setState({
+          isSignedIn: true,
+          isLoaded: true,
+        });
+      }
+    });
+  }
+
+  render() {
+    const { isSignedIn, isLoaded } = this.state;
+    if (!isLoaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+    if (!isSignedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <Text>User is Signed In</Text>
+      </View>
+    );
+  }
 }
