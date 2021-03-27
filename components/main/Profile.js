bd7579482bd18deb8437404aa4e6fc757b590300
Ilya <ilya.popov.sbmt@gmail.com>
2021-03-27 22:22:13 +0100

    Initial profile view added

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  View, Text, Image, FlatList, StyleSheet,
+} from 'react-native';
 
-export default function Profile() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+  },
+  gallery: {
+    flex: 1,
+  },
+  image: {
+    aspectRatio: 1 / 1,
+    flex: 1,
+    height: 200,
+  },
+  info: {
+    margin: 20,
+  },
+});
+function Profile(props) {
+  const { currentUser, posts } = props;
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.info}>
+        <Text>{currentUser.name}</Text>
+        <Text>{currentUser.email}</Text>
+      </View>
+      <View style={styles.gallery}>
+        <FlatList
+          numColumns={3}
+          horizontal={false}
+          data={posts}
+          renderItem={({ item }) => (
+            <Image
+              style={styles.image}
+              source={{ uri: item.imgUrl }}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+});
+
+export default connect(mapStateToProps, null)(Profile);
+
+// TODO: Change types checking with arrayOf and shape
+Profile.propTypes = {
+  currentUser: PropTypes.shape(React.propTypes).isRequired,
+  posts: PropTypes.array.isRequired,
+};
